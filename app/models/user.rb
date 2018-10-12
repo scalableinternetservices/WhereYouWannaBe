8fecfee13df048d584d423fd93676724850eaa9a
ap1995 <ashwinip7995@gmail.com>
2018-10-12 00:09:44 -0700

    Added validation for password and presence of parts of form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
+
+	include ActiveModel::Validations
+
+	attr_reader :name, :age, :email
+
+	validates :name, presence: true
+	validates :age, numericality: true 
+  validates :email, presence: true, format: { with: /A[^@s] @[^@s] z/, allow_blank: true }
+
 	has_many :events
 	has_many :comments
 	has_many :attendees
