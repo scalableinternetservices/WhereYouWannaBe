c47b1f77348204525ca2ec9dfc3f6a761f8a2f35
Santhu <santhameena13@gmail.com>
2018-10-16 11:10:41 -0700

    Change event creator_id to user_id in tests.

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -17,7 +17,7 @@ class EventsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create event" do
     assert_difference('Event.count') do
-      post events_url, params: { event: { creator_id: @event.creator_id, date: @event.date, description: @event.description, location_id: @event.location_id, price: @event.price, tag_id: @event.tag_id, title: @event.title } }
+      post events_url, params: { event: { user_id: @event.user_id, date: @event.date, description: @event.description, location_id: @event.location_id, price: @event.price, tag_id: @event.tag_id, title: @event.title } }
     end
 
     assert_redirected_to event_url(Event.last)
@@ -34,7 +34,7 @@ class EventsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update event" do
-    patch event_url(@event), params: { event: { creator_id: @event.creator_id, date: @event.date, description: @event.description, location_id: @event.location_id, price: @event.price, tag_id: @event.tag_id, title: @event.title } }
+    patch event_url(@event), params: { event: { user_id: @event.user_id, date: @event.date, description: @event.description, location_id: @event.location_id, price: @event.price, tag_id: @event.tag_id, title: @event.title } }
     assert_redirected_to event_url(@event)
   end
 
