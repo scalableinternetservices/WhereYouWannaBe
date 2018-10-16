9aab708e2479350119a387db08876f12bc2905eb
nikhilmaram <nike.maram@gmail.com>
2018-10-15 22:13:49 -0700

    Added email validation to the locations

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,8 @@
 class Location < ApplicationRecord
   has_many :event
-
+  validates:email, presence:true, format: {
+    with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,
+    message: 'Only valid emails allowed'
+    }
+	
 end
