9cf499a243ca089608a62bd001c8abd92af67d15
nikhilmaram <nike.maram@gmail.com>
2018-11-06 17:28:20 -0800

    relationship model follwed/following users

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,14 @@ class User < ApplicationRecord
                                   foreign_key: "follower_id",
                                   dependent:   :destroy
 
+	has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+
+	has_many :following, through: :active_relationships, source: :followed
+
+	has_many :followers, through: :passive_relationships, source: :follower
+
 	validates:name, presence:true
 	validates:city, presence:true
 	validates_numericality_of :age, greater_than: -1
@@ -21,4 +29,20 @@ class User < ApplicationRecord
     validates_uniqueness_of   :email
 	has_secure_password
 	# validates :password, presence: true, length: { minimum: 6 }
+
+
+	# Follows a user.
+  def follow(other_user)
+    following << other_user
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
+  end
 end
