84fab93d48337722016ad372a7f91e8c44e9a9cf
nikhilmaram <nike.maram@gmail.com>
2018-11-07 18:59:33 -0800

    follow and unfollow user buttons and count of following/followers in user profile page

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ User.create!(name:  "Example User",
              password:              "foobar",
              password_confirmation: "foobar",
              admin: true)
-
+ 
 99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
@@ -33,3 +33,11 @@ end
                description: description
               )
 end
+
+# Following relationships
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
