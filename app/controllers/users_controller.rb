9f8ef16dcbd72321ff818d3b951fd178a9716a5a
ap1995 <ashwinip7995@gmail.com>
2018-11-20 13:39:02 -0800

    Trying to remove pagination for load testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -74,14 +74,16 @@ class UsersController < ApplicationController
   def following
     @title = "Following"
     @user  = User.find(params[:id])
-    @users = @user.following.paginate(page: params[:page])
+    @users = @user.following.all
+    # @users = @user.following.paginate(page: params[:page])
     render 'show_follow'
   end
 
   def followers
     @title = "Followers"
     @user  = User.find(params[:id])
-    @users = @user.followers.paginate(page: params[:page])
+    @users = @user.followers.all
+    # @users = @user.followers.paginate(page: params[:page])
     render 'show_follow'
   end
 
