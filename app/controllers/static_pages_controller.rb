541117ce2c3f13769d4fb9ac02fa860defaf802d
chandana22 <chandanaupadhyaya22@gmail.com>
2018-11-26 11:13:43 +0000

    Event Recommendation bug fix, Tsung XML for static pages, updating static pages, added contact page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
-    @exclude_events = Attendee.select('event_id').where("user_id = ?",current_user.id)
     if (!current_user.nil? && !current_user.city.nil?)
+      @exclude_events = Attendee.select('event_id').where("user_id = ?",current_user.id)
       @events = Event.joins(:location).where("date > ? AND locations.city = ? AND events.id NOT IN (?)", DateTime.now, current_user.city,@exclude_events)
       if @events.length == 0
         @events = Event.where("date > ? AND events.id NOT IN (?)", DateTime.now,@exclude_events)
       end
     else
-      @events = Event.where("date > ? AND events.id NOT IN (?)", DateTime.now,@exclude_events)
+      @events = Event.where("date > ? ", DateTime.now)
     end 
   end
 
