3b80e2cc16bc720f8c2dec602472eb4a0a74639d
ap1995 <ashwinip7995@gmail.com>
2018-11-28 19:05:31 -0800

    Add Image URL validity to Without Pagination branch

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -2,4 +2,8 @@ module EventsHelper
     def google_map(center)
         "https://maps.googleapis.com/maps/api/staticmap?center=#{center}&size=300x300&zoom=17"
     end
+
+    def remote_file_exists?(url)
+      !url.match?(/^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.jpg)?$/ix)
+    end
 end
