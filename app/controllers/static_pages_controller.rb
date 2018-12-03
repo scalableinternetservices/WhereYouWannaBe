f3240617e70e950f7eb2b205a601fddaa2de09d8
Santhu <santhameena13@gmail.com>
2018-12-02 22:55:43 -0800

    Add query optimizations.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,12 +2,12 @@ class StaticPagesController < ApplicationController
   def home
     if (!current_user.nil? && !current_user.city.nil?)
       @exclude_events = Attendee.select('event_id').where("user_id = ?",current_user.id)
-      @events = Event.joins(:location).where("date > ? AND locations.city = ? AND events.id NOT IN (?)", DateTime.now, current_user.city,@exclude_events).paginate(page: params[:page],per_page:10)
+      @events = Event.includes(:location).includes(:tag).joins(:location).where("date > ? AND locations.city = ? AND events.id NOT IN (?)", DateTime.now, current_user.city,@exclude_events).paginate(page: params[:page],per_page:10)
       if @events.length == 0
-        @events = Event.where("date > ? AND events.id NOT IN (?)", DateTime.now,@exclude_events).paginate(page: params[:page],per_page:10)
+        @events = Event.includes(:location).includes(:tag).where("date > ? AND events.id NOT IN (?)", DateTime.now,@exclude_events).paginate(page: params[:page],per_page:10)
       end
     else
-      @events = Event.where("date > ? ", DateTime.now).paginate(page: params[:page],per_page:10)
+      @events = Event.includes(:location).includes(:tag).where("date > ? ", DateTime.now).paginate(page: params[:page],per_page:10)
     end
   end
 
